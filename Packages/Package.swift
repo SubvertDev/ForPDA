708045c98e6ab105bc3dc90d0b3770e688e4f445
Ilia Lubianoi <subvert.dev@gmail.com>
2024-12-15 23:11:24 +0300

    Update packages

--- Packages/Package.swift
+++ Packages/Package.swift
@@ -46,21 +46,21 @@ let package = Package(
         .library(name: "TCAExtensions", targets: ["TCAExtensions"])
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "1.16.0"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "1.17.0"),
         .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols.git", from: "5.3.0"),
         .package(url: "https://github.com/hyperoslo/Cache.git", from: "7.3.0"),
         .package(url: "https://github.com/kean/Nuke.git", from: "12.8.0"),
-        .package(url: "https://github.com/PostHog/posthog-ios", from: "3.15.7"),
-        .package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "8.39.0"),
+        .package(url: "https://github.com/PostHog/posthog-ios", from: "3.17.0"),
+        .package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "8.41.0"),
         .package(url: "https://github.com/CSolanaM/SkeletonUI.git", from: "2.0.2"),
         .package(url: "https://github.com/raymondjavaxx/SmoothGradient.git", branch: "main"),
         .package(url: "https://github.com/SvenTiigi/YouTubePlayerKit.git", from: "1.9.0"),
         .package(url: "https://github.com/SubvertDev/AlertToast.git", revision: "d0f7d6b"),
         .package(url: "https://github.com/kirualex/SwiftyGif.git", from: "5.4.4"),
-        .package(url: "https://github.com/ZhgChgLi/ZMarkupParser.git", from: "1.11.0"),
+        .package(url: "https://github.com/ZhgChgLi/ZMarkupParser.git", from: "1.12.0"),
         .package(url: "https://github.com/SubvertDev/PDAPI_SPM.git", from: "0.2.0"),
         .package(url: "https://github.com/SubvertDev/RichTextKit.git", branch: "main"),
-        .package(url: "https://github.com/exyte/Chat.git", from: "2.0.8")
+        .package(url: "https://github.com/exyte/Chat.git", from: "2.0.10")
     ],
     targets: [
         
