dc97da2e81f2680e38370d1d738550f2b9cdfe32
Ilia Lubianoi <subvert.dev@gmail.com>
2024-12-27 16:17:51 +0300

    Add animated icons

--- App/App.swift
+++ App/App.swift
@@ -18,12 +18,39 @@ struct ForPDAApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     @Environment(\.scenePhase) private var scenePhase
     
+    // Remove after New Year
+    @State var backgroundTask: UIBackgroundTaskIdentifier? = nil
+    let iconAnimator = IconAnimator(
+        numberOfFrames: 90,
+        numberOfLoops: 3,
+        targetFramesPerSecond: 30,
+        shouldRunOnMainThread: true
+    )
+    
     var body: some Scene {
         WindowGroup {
             if TestContext.current == nil {
                 AppView(store: appDelegate.store)
                     .onChange(of: scenePhase) { newScenePhase in
                         appDelegate.store.send(.scenePhaseDidChange(from: scenePhase, to: newScenePhase))
+                        
+                        // Remove after New Year
+                        switch newScenePhase {
+                        case .active:
+                            endTask()
+                        case .background:
+                            print("Starting animation")
+                            backgroundTask = UIApplication.shared.beginBackgroundTask()
+                            iconAnimator.startAnimation() {
+                                print("Finishing animation")
+                                // Once the animation is complete,
+                                // end our background task so that iOS knows
+                                // that our app has finished its work
+                                endTask()
+                            }
+                        default:
+                            break
+                        }
                     }
                     .onOpenURL { url in
                         appDelegate.store.send(.deeplink(url))
@@ -34,6 +61,14 @@ struct ForPDAApp: App {
         .backgroundTask(.appRefresh(appDelegate.store.notificationsId)) { _ in
             await appDelegate.store.send(.syncUnreadTaskInvoked)
         }
+    }
+    
+    // Remove after New Year
+    private func endTask() {
+        if let backgroundTask = backgroundTask {
+            UIApplication.shared.endBackgroundTask(backgroundTask)
+        }
+        backgroundTask = nil
     }
 }
 
