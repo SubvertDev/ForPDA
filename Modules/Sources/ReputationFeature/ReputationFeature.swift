dbfcefb2e2fabba33fad56453cca75dc3779a257
Рустам Ойтов <rustamoytov@Rustams-MacBook-Pro.local>
2025-07-11 21:08:16 +0300

    Add SegmentPicker

--- Modules/Sources/ReputationFeature/ReputationFeature.swift
+++ Modules/Sources/ReputationFeature/ReputationFeature.swift
@@ -7,31 +7,68 @@
 
 import Foundation
 import ComposableArchitecture
+import APIClient
 
 @Reducer
 public struct ReputationFeature: Reducer, Sendable {
     
     public init() {}
     
+    enum PickerSelection: Int {
+        case history = 1
+        case votes = 2
+    }
+    
     // MARK: - State
     
     @ObservableState
     public struct State: Equatable {
+        
+        public var isLoading = false
+        var pickerSelection: PickerSelection = .history
+        
         public init() {}
+        
     }
     
     // MARK: - Action
-    public enum Action: ViewAction {
+    public enum Action: BindableAction, ViewAction {
+        case binding(BindingAction<State>)
+        
         case view(View)
         public enum View {
             case onAppear
         }
+        
+        case `internal`(Internal)
+        public enum Internal {
+            case loadHistory
+        }
     }
     
+    // MARK: - Dependencies
+    
+    @Dependency(\.apiClient) private var apiClient
+    
     public var body: some Reducer<State, Action> {
+        BindingReducer()
+
         Reduce<State, Action> {state, action in
             switch action {
             case .view(.onAppear):
+                if state.pickerSelection == .history {
+                    print("is onAppear history")
+                    
+                } else {
+                    print("is onappear votes")
+                }
+            return .none
+                
+            case .internal(.loadHistory):
+                
+                return .none
+                
+            case .binding:
                 return .none
             }
         }
--- Modules/Sources/ReputationFeature/ReputationScreen.swift
+++ Modules/Sources/ReputationFeature/ReputationScreen.swift
@@ -28,10 +28,80 @@ public struct ReputationScreen: View {
                 Color(.Background.primary)
                     .ignoresSafeArea()
                 
-                Text("Reputation view")
+                VStack {
+                    SegmentPicker()
+                    if !store.isLoading {
+                        switch store.pickerSelection {
+                        case .history:
+                            HistorySelection()
+                                .onAppear {
+                                    send(.onAppear)
+                                }
+                        case .votes:
+                            Text("vot")
+                                .onAppear {
+                                    send(.onAppear)
+                                }
+                        }
+                    } else {
+                        Spacer()
+                        PDALoader()
+                            .frame(width: 24, height: 24)
+                        Spacer()
+                    }
+                }
             }
             .navigationTitle(Text("Reputation", bundle: .module))
             .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+    
+    @ViewBuilder
+    private func HistorySelection() -> some View {
+        ZStack {
+            Color(.Background.primary)
+                .ignoresSafeArea()
+            
+        }
+    }
+    
+    // MARK: - SegmentPicker
+    
+    @ViewBuilder
+    private func SegmentPicker() -> some View {
+        _Picker("", selection: $store.pickerSelection) {
+            Text("History", bundle: .module)
+                .tag(ReputationFeature.PickerSelection.history)
+            
+            Text("Votes", bundle: .module)
+                .tag(ReputationFeature.PickerSelection.votes)
+        }
+        .pickerStyle(.segmented)
+    }
+}
+
+// MARK: - Perception Picker
+// https://github.com/pointfreeco/swift-perception/issues/100
+
+struct _Picker<Label, SelectionValue, Content>: View
+where Label: View, SelectionValue: Hashable, Content: View {
+    let label: Label
+    let content: Content
+    let selection: Binding<SelectionValue>
+    
+    init(
+        _ titleKey: String,
+        selection: Binding<SelectionValue>,
+        @ViewBuilder content: () -> Content
+    ) where Label == Text {
+        self.label = Text(titleKey)
+        self.content = content()
+        self.selection = selection
+    }
+    
+    var body: some View {
+        _PerceptionLocals.$skipPerceptionChecking.withValue(true) {
+            Picker(selection: selection, content: { content }, label: { label })
         }
     }
 }
