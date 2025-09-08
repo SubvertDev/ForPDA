67ad1ae99f9f874e8fab57a4089286d19fe07103
Ilia Lubianoi <subvert.dev@gmail.com>
2025-09-09 00:52:20 +0300

    Bump minimum Xcode version to 26

--- Tuist.swift
+++ Tuist.swift
@@ -2,7 +2,7 @@ import ProjectDescription
 
 let tuist = Tuist(
     project: .tuist(
-        compatibleXcodeVersions: .list(["16.4.0", "26.0.0"]),
+        compatibleXcodeVersions: .upToNextMajor("26.0.0"),
         swiftVersion: "6.1.2",
     )
 )
