1f3c0eb41a4b61ffb398d52e68bee36a9a36fb72
Ilia Lubianoi <subvert.dev@gmail.com>
2025-09-27 18:06:24 +0300

    Connect tuist server features

--- Tuist.swift
+++ Tuist.swift
@@ -1,6 +1,7 @@
 import ProjectDescription
 
 let tuist = Tuist(
+    fullHandle: "forpda/forpda",
     project: .tuist(
         compatibleXcodeVersions: .upToNextMajor("26.0.0"),
         swiftVersion: "6.1.2",
