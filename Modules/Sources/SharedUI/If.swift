f75000e5e161411c569750aa446b221cefe4225e
Ilia Lubianoi <subvert.dev@gmail.com>
2025-11-30 18:34:36 +0300

    Add new if/else conditional modifier

--- Modules/Sources/SharedUI/If.swift
+++ Modules/Sources/SharedUI/If.swift
@@ -8,12 +8,26 @@
 import SwiftUI
 
 public extension View {
+    
     @ViewBuilder
     func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
         if condition {
             transform(self)
         } else {
             self
+        }
+    }
+    
+    @ViewBuilder
+    func ifElse<TrueContent: View, FalseContent: View>(
+        _ condition: Bool,
+        @ViewBuilder trueCondition: (Self) -> TrueContent,
+        @ViewBuilder falseCondition: (Self) -> FalseContent
+    ) -> some View {
+        if condition {
+            trueCondition(self)
+        } else {
+            falseCondition(self)
         }
     }
 }
