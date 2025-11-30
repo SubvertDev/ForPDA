a07e77cd358795a54e4d7fc4e127313ba61d523f
Xialtal <max352516@gmail.com>
2025-11-30 17:28:09 +0300

    Add UITitleType model

---
+++ Modules/Sources/SharedUI/UITitleType.swift
@@ -0,0 +1,13 @@
+//
+//  UITitleType.swift
+//  ForPDA
+//
+//  Created by Xialtal on 30.11.25.
+//
+
+import SwiftUI
+
+public enum UITitleType {
+    case plain(String)
+    case render(NSAttributedString)
+}
