7ebc72f99efaf2a46c7e2691e686c13bcdfa0fbd
Ilia Lubianoi <subvert.dev@gmail.com>
2025-12-14 13:50:18 +0300

    Make PageNavigation fit it's field size

--- Modules/Sources/PageNavigationFeature/PageNavigationView.swift
+++ Modules/Sources/PageNavigationFeature/PageNavigationView.swift
@@ -12,12 +12,6 @@ import Models
 import SharedUI
 import Perception
 
-extension Binding where Value == Bool {
-    static func orFalse(_ binding: Binding<Bool>?) -> Binding<Bool> {
-        binding ?? .constant(false)
-    }
-}
-
 public struct PageNavigation: View {
     
     // MARK: - Properties
@@ -79,51 +73,28 @@ public struct PageNavigation: View {
                 
                 Spacer()
                 
-                HStack(spacing: 6) {
-                    TextField(String(""), text: $store.page)
-                        .font(.subheadline.monospacedDigit())
-                        .keyboardType(.numberPad)
-                        .focused($focus, equals: PageNavigationFeature.State.Field.page)
-                        .fixedSize()
-                        .onChange(of: store.page) { newValue in
-                            guard !store.page.isEmpty else { return }
-                            store.page = String(min(Int(store.page.filter(\.isNumber))!, store.totalPages))
-                        }
-                        .toolbar {
-                            if focus == .page {
-                                ToolbarItemGroup(placement: .keyboard) {
-                                    Spacer()
-                                    
-                                    Button {
-                                        store.send(.doneButtonTapped)
-                                    } label: {
-                                        Text("Done", bundle: .module)
-                                    }
-                                }
-                            }
-                        }
-                        .opacity(store.focus == nil ? 0 : 1)
-                        .overlay {
-                            Text(store.page)
-                                .font(.subheadline.monospacedDigit())
-                                .fixedSize()
-                                .opacity(store.focus == nil ? 1 : 0)
-                                .contentTransition(.numericText())
-                                .animation(.default, value: store.page)
-                        }
+                ViewThatFits(in: .horizontal) {
+                    HStack(spacing: 6) {
+                        PageField()
+                        
+                        Text(verbatim: "/")
+                            .font(.subheadline.monospacedDigit())
+                        
+                        Text(verbatim: "\(store.totalPages)")
+                            .font(.subheadline.monospacedDigit())
+                    }
                     
-                    Text(verbatim: "/")
-                        .font(.subheadline.monospacedDigit())
-                    
-                    Text(verbatim: "\(store.totalPages)")
-                        .font(.subheadline.monospacedDigit())
+                    PageField()
                 }
                 .padding(.vertical, 6)
                 .padding(.horizontal, 12)
                 .background(
                     RoundedRectangle(cornerRadius: 8)
                         .foregroundStyle(Color(.Background.teritary))
-                        .glassEffect(.identity)
+                        .if(!store.appSettings.experimentalFloatingNavigation) { content in
+                            content
+                                .glassEffect(.identity)
+                        }
                 )
                 .onTapGesture {
                     store.send(.onViewTapped)
@@ -172,6 +143,41 @@ public struct PageNavigation: View {
                 .frame(maxWidth: .infinity, alignment: .trailing)
                 .animation(.default, value: minimized)
         }
+    }
+    
+    @ViewBuilder
+    private func PageField() -> some View {
+        TextField(String(""), text: $store.page)
+            .font(.subheadline.monospacedDigit())
+            .keyboardType(.numberPad)
+            .focused($focus, equals: PageNavigationFeature.State.Field.page)
+            .fixedSize()
+            .onChange(of: store.page) { newValue in
+                guard !store.page.isEmpty else { return }
+                store.page = String(min(Int(store.page.filter(\.isNumber)) ?? 1, store.totalPages))
+            }
+            .toolbar {
+                if focus == .page {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer()
+                        
+                        Button {
+                            store.send(.doneButtonTapped)
+                        } label: {
+                            Text("Done", bundle: .module)
+                        }
+                    }
+                }
+            }
+            .opacity(store.focus == nil ? 0 : 1)
+            .overlay {
+                Text(store.page)
+                    .font(.subheadline.monospacedDigit())
+                    .fixedSize()
+                    .opacity(store.focus == nil ? 1 : 0)
+                    .contentTransition(.numericText())
+                    .animation(.default, value: store.page)
+            }
     }
     
     // MARK: - Legacy Navigation
@@ -280,7 +286,7 @@ public struct PageNavigation: View {
 // MARK: - Previews
 
 @available(iOS 17, *)
-#Preview {
+#Preview("Base navigation") {
     @Previewable @State var store = Store(
         initialState: PageNavigationFeature.State(type: .forum)
     ) {
@@ -289,7 +295,6 @@ public struct PageNavigation: View {
     @Previewable @State var minimized = false
     
     @Shared(.appSettings) var appSettings
-    let _ = $appSettings.floatingNavigation.withLock { $0 = true }
     
     ZStack {
         LinearGradient(
@@ -314,6 +319,65 @@ public struct PageNavigation: View {
         }
     }
     .onAppear {
+        $appSettings.floatingNavigation.withLock { $0 = true }
         store.send(.update(count: 50000, offset: 0))
+    }
+}
+
+@available(iOS 26.2, *)
+#Preview("Experimental navigation") {
+    @Previewable @State var store = Store(
+        initialState: PageNavigationFeature.State(type: .forum)
+    ) {
+        PageNavigationFeature()
+    }
+    
+    @Shared(.appSettings) var appSettings
+
+    WithPerceptionTracking {
+        TabView {
+            Tab {
+                ZStack {
+                    LinearGradient(
+                        colors: [.red, .blue],
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
+                    .ignoresSafeArea()
+                    
+                    ScrollView(.vertical) {
+                        ForEach(0..<100) { index in
+                            Text(String(index))
+                        }
+                        .frame(maxWidth: .infinity)
+                    }
+                    .overlay {
+                        VStack {
+                            Button(String("Enable")) {
+                                store.send(.update(count: 50000, offset: 50000-20))
+                            }
+                            Button(String("Disable")) {
+                                store.send(.update(count: 0, offset: 0))
+                            }
+                        }
+                        .background(.white)
+                    }
+                }
+            } label: {
+                Label(String("Test"), systemSymbol: .paperclip)
+            }
+            
+            Tab {}; Tab {}; Tab {}
+        }
+        .tabViewBottomAccessory(isEnabled: store.totalPages > 1) {
+            PageNavigation(store: store)
+        }
+        .tabBarMinimizeBehavior(.onScrollDown)
+        .onAppear {
+            $appSettings.floatingNavigation.withLock { $0 = true }
+            $appSettings.experimentalFloatingNavigation.withLock { $0 = true }
+            store.send(.update(count: 50000, offset: 50000-20))
+        }
+        .animation(.default, value: store.count)
     }
 }
