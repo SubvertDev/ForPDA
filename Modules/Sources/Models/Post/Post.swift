0dcf2a24cef230e066c26bfdfc8a47d9b79d3396
Xialtal <max352516@gmail.com>
2026-04-08 21:27:43 +0300

    Improve Post model flag mock

--- Modules/Sources/Models/Post/Post.swift
+++ Modules/Sources/Models/Post/Post.swift
@@ -159,7 +159,7 @@ public extension Post {
     static func mock(id: Int = 0) -> Post {
         return Post(
             id: id,
-            flag: [.canDelete, .canEdit, .canModerate],
+            flag: [.closed, .hidden, .canDelete, .canEdit, .canModerate, .canProtect],
             content: "[snapback]123[/snapback], Lorem ipsum...\n[font=fontello]4[/font]",
             author: Author(
                 id: 6176341,
