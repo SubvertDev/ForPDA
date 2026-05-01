4c1cd453b4d062d67aaf59de1d18c275496d950b
Xialtal <max352516@gmail.com>
2026-05-01 18:35:00 +0300

    Add more error reasons to TopicEditResponse

--- Modules/Sources/Models/Forum/TopicEditResponse.swift
+++ Modules/Sources/Models/Forum/TopicEditResponse.swift
@@ -9,6 +9,8 @@ public enum TopicEditResponse: Int, Sendable {
     case success = 0
     case tooManyQuestionsInPoll = 4
     case tooManyAnswersInPoll = 5
+    case inappropriateContent = 6
+    case sentToPremod = 7
     case noAccess
     
     public init(rawValue: Int) {
@@ -16,6 +18,8 @@ public enum TopicEditResponse: Int, Sendable {
         case 0: self = .success
         case 4: self = .tooManyQuestionsInPoll
         case 5: self = .tooManyAnswersInPoll
+        case 6: self = .inappropriateContent
+        case 7: self = .sentToPremod
         default: self = .noAccess
         }
     }
