9d03f66c33db9dc6eef983e4bd5dfed19c1ddd29
Xialtal <max352516@gmail.com>
2026-05-15 17:27:24 +0300

    Fix crash on open post/topic history

--- Modules/Sources/Models/Forum/ForumEventLogType.swift
+++ Modules/Sources/Models/Forum/ForumEventLogType.swift
@@ -5,7 +5,7 @@
 //  Created by Xialtal on 14.05.26.
 //
 
-public enum ForumEventLogType: Int, Sendable {
+public enum ForumEventLogType: Int, Sendable, Hashable, Equatable {
     case post  = 1
     case topic = 0
 }
